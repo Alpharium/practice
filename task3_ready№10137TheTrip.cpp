f2e204d6cabb526378c051662f72f6883553baec
Alpharium <35428239+Alpharium@users.noreply.github.com>
2018-01-28 03:51:52 +0500

    Update task3_ready№10137TheTrip.cpp

--- task3_ready№10137TheTrip.cpp
+++ task3_ready№10137TheTrip.cpp
@@ -1,5 +1,5 @@
-//please give me a job
-/* The Trip - "Ïóòåøåñòâèÿ" Input
+/* The Trip - "ĞŸÑƒÑ‚ĞµÑˆĞµÑÑ‚Ğ²Ğ¸Ñ" 
+Input
 Standard input will contain the information for several trips. The information for each trip consists of
 a line containing a positive integer, n, the number of students on the trip, followed by n lines of input,
 each containing the amount, in dollars and cents, spent by a student. There are no more than 1000
@@ -7,7 +7,7 @@ students and no student spent more than $10,000.00. A single line containing 0 f
 for the last trip.
 Output
 For each trip, output a line stating the total amount of money, in dollars and cents, that must be
-exchanged to equalize the students’ costs.
+exchanged to equalize the studentsâ€™ costs.
 */
 
 
