0d0c3da74bd7fdde2fbe5d691e010e499842d2d7
Alpharium <35428239+Alpharium@users.noreply.github.com>
2018-01-28 03:48:11 +0500

    Тестовое задание

---
+++ task1_ready№377.cpp
@@ -0,0 +1,68 @@
+//will work for food
+/*На числовой прямой окрасили N отрезков. Известны координаты левого и правого концов каждого отрезка (Li и Ri). Найти длину окрашенной части числовой прямой.
+Входные данные
+В первой строке находится число N, в следующих N строках - пары Li и Ri. Li и Ri - целые, -1 000 000 000 <= Li <= Ri <= 1 000 000 000, 1 <= N <= 15 000 
+Выходные данные
+Вывести одно число - длину окрашенной части прямой. 
+
+
+
+объединяю перекрывающиеся отрезки,
+затем считаю сумму всех длин неперекрывающихся отрезков
+*/
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+bool pairCompare(const std::pair<int, int>& firstElem, const std::pair<int, int>&secondElem) {
+	return firstElem.first < secondElem.first;
+}
+int main()
+{
+	std::vector <std::pair <int, int>> mass;
+	std::vector <int> lenghts;
+	int n_mas;
+	std::cin >> n_mas;
+	mass.reserve(n_mas);
+	for (int i = 0;i < n_mas;i++) {
+		int left, right;
+		std::cin >> left >> right;
+		mass.push_back({ left,right });
+	}
+	int left, right;
+	std::sort(mass.begin(), mass.end(), pairCompare);
+	int lenght = 0;
+	int cycler = 0;
+	for (int n = 0;n < n_mas ;) {
+		left = mass[n].first;
+		right = mass[n].second;
+		for (int k = n;k < n_mas;k++) {
+			if (mass[k].first > right) {
+				break;
+			}
+			cycler++;
+			if (mass[k].first<=right) {
+				if (mass[k].second > right) {
+					right = mass[k].second;
+				}
+			}
+
+		}
+		lenght += right - left;
+		lenghts.push_back(lenght);
+		lenght = 0;
+		n = n + cycler;
+		cycler = 0;
+
+	}
+	int result = 0;
+
+	for (int i = 0;i < lenghts.size();i++) {
+		result += lenghts[i];
+	}
+	std::cout << "\n\n" << result << "\n";
+	getchar();
+	getchar();
+	return 0;
+}
---
+++ task2_ready№619.cpp
@@ -0,0 +1,53 @@
+//will code for food
+/*Кубик, грани которого помечены цифрами от 1 до 6, бросают N раз. Найти вероятность того, что сумма выпавших чисел будет равна Q.
+Ограничения: 1 <= N <= 500, 1 <= Q <= 3000.
+Входные данные
+В первой строке находятся числа N и Q через пробел.
+Выходные данные
+Вероятность того, что сумма выпавших чисел будет равна Q.
+
+сумма вероятностей выпадения значений на кубике умноженых на вероятность того что на предыдущем шаге значение суммы отличалось именно на выпавшее значение на кубике на текущем шаге
+
+*/
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+
+double funct(int n, int q) {
+	std::vector<double> mass(q + 1, 0);
+	mass[0] = 1;
+	std::vector<double> temp(q + 1, 0);
+	if (q < n) return 0;
+	if ((6 * n) < q) return 0;
+	for (int x = 1; x <= n;x++) {
+		mass.swap(temp);
+		for (int y = 0;y <= q;y++) {
+			mass[y] = 0;
+			for (int value = 1;value <= 6;value++) {
+				if (y - value >= 0) {
+					mass[y] = temp[y - value] + mass[y];
+				}
+			}
+			mass[y] /= 6;
+		}
+	}
+	return mass[q];
+};
+int main()
+{
+	std::cout << "\n 1<=N<=500; 1<=Q<=3000\n\n\n";
+	int a, b;
+	std::cin >> a >> b;
+	std::cout << std::setprecision(40) << std::scientific << funct(a, b);
+	
+	
+	
+	getchar();
+	getchar();
+	return 0;
+}
